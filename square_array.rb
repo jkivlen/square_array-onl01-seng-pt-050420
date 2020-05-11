1f7a4f9371e199927f6555e859c5dc7f8890d586
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-11 17:28:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
- array = [1, 2, 3]
+ 
+ new_array = []
 
  array.each do |integer|
-  array << integer ** 2
-
-  end
+  new_array << integer ** 2
+ end
+ return new_array
 end
